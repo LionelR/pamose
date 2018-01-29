e1810cdf3cd77177cc77a1df8bf006febea01d6e
LionelR <lionel.roubeyrie@codinux.fr>
2018-01-29 23:11:58 +0100

    models completion

--- pamose/models.py
+++ pamose/models.py
@@ -2,8 +2,13 @@ import time
 from .extensions import db
 import sqlalchemy as db  # auto-completion
 
-
 # Many-to-many secondary (central) tables definition
+user_usergroup_table = db.Table('user_usergroup',
+                                db.Model.metadata,
+                                db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                                db.Column('user_group_id', db.Integer, db.ForeignKey('user_group.id'), primary_key=True)
+                                )
+
 role_right_table = db.Table('role_right',
                             db.Model.metadata,
                             db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
@@ -31,6 +36,16 @@ class User(Base):
     token = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=True)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
+    role = db.relationship('Role', backref='users', lazy=True)
+    user_groups = db.relationship('UserGroup', secondary=user_usergroup_table, backref='users', lazy=True)
+
+
+class UserGroup(Base):
+    """
+    Users groups
+    """
+    __tablename__ = 'user_group'
+    name = db.Column(db.String, unique=True, nullable=False)
 
 
 class Role(Base):
@@ -51,22 +66,6 @@ class Right(Base):
     name = db.Column(db.String, unique=True, nullable=False)
 
 
-class Tag(Base):
-    """
-    Available tags
-    """
-    __tablename__ = 'tag'
-    name = db.Column(db.String, unique=True, nullable=False)
-
-
-class EntityType(Base):
-    """
-    List of entities types (Realm, Host, Service, ...)
-    """
-    __tablename__ = 'entity_type'
-    name = db.Column(db.String, unique=True, nullable=False)
-
-
 class Entity(Base):
     """
     Central table for describing to logical hierarchy of monitored objects
@@ -83,27 +82,49 @@ class Entity(Base):
     # parent = db.relationship("Entity", remote_side=[id])
     childs = db.relationship("Entity", backref=db.backref('parent', remote_side=[id]))
     livestates = db.relationship('Livestate', backref='entity', lazy=True)
-    auto_acknowledge = db.Column(db.Boolean, nullable=False, default=False)
-
-
-class StateSeverity(Base):
-    """
-    States severities levels
-    """
-    __tablename__ = 'state_severity'
-    severity = db.Column(db.Integer, unique=True, nullable=False)
-    description = db.Column(db.String, unique=False, nullable=False)
+    is_auto_acknowledge = db.Column(db.Boolean, nullable=False, default=False)
+    is_template = db.Column(db.Boolean, nullable=False, default=False)
+    is_activated = db.Column(db.Boolean, nullable=False, default=False)
+    is_auto_expirable = db.Column(db.Boolean, nullable=False, default=True)
+    auto_expirable_interval = db.Column(db.Integer, nullable=False, default=0)
 
 
 class State(Base):
     """
-    List of possible states (OK, UP, DOWN, ...)
+    List of possible states (EXPIRED, OK, UP, DOWN, ...) by Entity Type
     """
     __tablename__ = 'state'
     name = db.Column(db.String, unique=True, nullable=False)
-    severity_id = db.Column(db.Integer, db.ForeignKey('state_severity.id'))
+    severity_id = db.Column(db.Integer, db.ForeignKey('severity.id'))
+    severity = db.relationship("Severity", backref='states')
     entity_type_id = db.Column(db.Integer, db.ForeignKey('entity_type.id'))
     entity_type = db.relationship("EntityType", backref='states')
+
+
+class Severity(Base):
+    """
+    States severities levels
+    """
+    __tablename__ = 'severity'
+    name = db.Column(db.String, unique=True, nullable=False)
+    value = db.Column(db.Integer, unique=True, nullable=False)
+    description = db.Column(db.String, unique=False, nullable=True)
+
+
+class Tag(Base):
+    """
+    Available tags
+    """
+    __tablename__ = 'tag'
+    name = db.Column(db.String, unique=True, nullable=False)
+
+
+class EntityType(Base):
+    """
+    List of entities types (Realm, Host, Service, ...)
+    """
+    __tablename__ = 'entity_type'
+    name = db.Column(db.String, unique=True, nullable=False)
 
 
 class Livestate(Base):
@@ -119,16 +140,8 @@ class Livestate(Base):
     timestamp = db.Column(db.DateTime, default=time.time())
     output = db.Column(db.String, unique=False, nullable=True)
     longoutput = db.Column(db.String, unique=False, nullable=True)
-    acknowledged = db.Column(db.Boolean, default=False, nullable=False)
+    is_acknowledged = db.Column(db.Boolean, default=False, nullable=False)
     metrics = db.relationship('Metric', backref='livestate', lazy=True)
-
-
-class MetricType(Base):
-    """
-    Possible metric type (standard or cumulative)
-    """
-    __tablename__ = 'metric_type'
-    name = db.Column(db.String, unique=True, nullable=False)
 
 
 class Metric(Base):
@@ -142,3 +155,11 @@ class Metric(Base):
     livestate_id = db.Column(db.Integer, db.ForeignKey('livestate.id'))
     metric_type_id = db.Column(db.Integer, db.ForeignKey('metric_type.id'))
     metric_type = db.relationship('MetricType', backref='metrics', lazy=True)
+
+
+class MetricType(Base):
+    """
+    Possible metric type (standard or cumulative)
+    """
+    __tablename__ = 'metric_type'
+    name = db.Column(db.String, unique=True, nullable=False)
